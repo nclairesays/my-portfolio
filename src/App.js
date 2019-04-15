3da6aa2cc6b1c6cd5b856cd56c79547145b8a298
disarrangedorder <42423542+disarrangedorder@users.noreply.github.com>
2019-04-14 23:39:57 -0700

    init commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,17 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-class App extends Component {
+import React, { Component } from 'react'
+import Button from '@material-ui/core/Button';
+import './styles.css'
+export default class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        test
+ 
+    <Button variant="contained" color="primary">
+      Hello World BUTTON
+    </Button>
+
       </div>
-    );
+    )
   }
 }
-
-export default App;
