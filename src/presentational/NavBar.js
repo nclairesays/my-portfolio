d2684b4e7756a2bc139ad12ba94d4c83c639cdb3
claire n <ms.clarewynn@gmail.com>
2019-04-19 23:12:50 -0700

    updated menu styles, will add animation later

--- src/presentational/NavBar.js
+++ src/presentational/NavBar.js
@@ -1,25 +1,54 @@
 import React, { Component } from 'react'
 // import ReactDOM from 'react-dom';
-import "./NavBar.css"
+import "./NavBar.scss"
 import { connect } from 'react-redux'
 import { toggleMenu } from './../redux/actions/navActionCreators';
 
 
 class NavBar extends Component {
 
-  onToggle = (e) => {
+  onToggle = () => {
     this.props.toggleMenu(!this.props.expanded)
   }
 
+
+  
+  onToggleBurger = (e) => {
+
+    // e.className.toggle("change");
+    console.log(e)
+    // return <div className="container" onclick={this.onToggleBurger}>
+    //           <div className="bar1"></div>
+    //           <div className="bar2"></div>
+    //           <div className="bar3"></div>
+    // </div>
+  }
+
+
+  
+
   render() {
+    const className = this.props.expanded ? 'x-container' : 'burger-container'
+    
     return (
       
+ 
       this.props.expanded 
-      ?
+      ? 
         <div id="nav-bar" >
+            <div className='x-container change' onClick={this.onToggle}> 
+                    <div className="bar1"></div>
+                    <div className="bar2"></div>
+                    <div className="bar3"></div>
+            </div>
           <ul className="nav-items">
+            {/* <li className='x-container change' onClick={this.onToggle}> 
+                    <div className="bar1"></div>
+                    <div className="bar2"></div>
+                    <div className="bar3"></div>
+            </li> */}
             {this.props.navItems.map( item => (
-              <li key={item} className="nav-item">
+              <li key={item}>
                 {item}
               </li>))
             }
@@ -29,7 +58,11 @@ class NavBar extends Component {
         </div>
       :
         <div id='nav-burger' onClick={this.onToggle}>
-
+            <div className={className}>
+              <div className="bar1"></div>
+              <div className="bar2"></div>
+              <div className="bar3"></div>
+            </div>
         </div>
 
      
