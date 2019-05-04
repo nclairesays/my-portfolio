529813a46e7f03a668f23acdce34d02ad52924c9
claire n <ms.clarewynn@gmail.com>
2019-05-04 00:52:56 -0700

    done with layout of form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,52 +1,51 @@
-import React, { Component } from 'react'
-import Slide from './Slide'
-import '../styles/components/Contact.scss'
+import React, { Component } from "react";
+import Slide from "./Slide";
+import "../styles/components/Contact.scss";
 class Contact extends Component {
   render() {
     return (
-      <Slide slideID={'Contact'} slideTitle={`Let's Talk!`} mainContent={this.mainContent()} />
-      
-      )
+      <Slide
+        slideID={"Contact"}
+        slideTitle={`Let's Connect!`}
+        mainContent={this.mainContent()}
+      />
+    );
   }
   mainContent = () => {
-    return this.contactForm()
-    // return  <div className={'profilepic2'} >
-    // currently getting errors with sass regarding setting background image to a local file.
-    // </div>
-  }
-  
+    return this.contactForm();
+  };
+
+  handleSubmit = (e) => { 
+    e.preventDefault()
+    console.log('something submited') 
+  };
   contactForm = () => {
-    return <div className="contact-form-container">
-    <h2 id="contact-title">Contact Us</h2>
-    <form method="POST" name="contact-form" aria-labelledby="contact">
-      <label htmlFor="name">Full Name</label>
-      <br />
-      <input id="name" name="name" className="contact-input" />
-      <br />
+    return (
+      <div className="contact-container">
+        <form onSubmit={() => this.handleSubmit()}>
+          <label htmlFor="name">Name</label>
+          <input id="name" name="name" className="contact-input" placeholder="Como te llamas?"/>
 
-      <label htmlFor="email">Email</label>
-      <br />
-      <input id="email" name="email" className="contact-input" />
-      <br />
+          <label htmlFor="email">Email</label>
+          <input id="email" name="email" className="contact-input" placeholder="So I can reply back!"/>
+          
+          <label htmlFor="phone">Phone</label>
+          <input id="phone" name="phone" className="contact-input"  placeholder="Calls may be faster..." />
 
-      <label htmlFor="phone">Phone</label>
-      <br />
-      <input id="phone" name="phone" className="contact-input" />
-      <br />
-      <label htmlFor="message">Message</label>
-      <br />
-      <textarea
-        id="message"
-        name="message"
-        className="contact-input"
-        form="contact-form"
-      />
+          <label htmlFor="message">Message</label>
+          <textarea
+            id="message"
+            name="message"
+            className="contact-input"
+            form="contact-form"
+            placeholder="Look forward to chatting! :)"
+          />
 
-      <input id="send-btn" type="submit" value="Send" />
-    </form>
-  </div>
-  }
-
+          <input id="submit-btn" type="submit" value="Send" />
+        </form>
+      </div>
+    );
+  };
 }
 
-export default Contact
+export default Contact;
