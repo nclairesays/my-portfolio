314bc8f170d4cb8f33afecb937ddcbb9d6614031
nclairesays <45305029+nclairesays@users.noreply.github.com>
2019-08-05 23:01:27 -0700

    updated project cards responsiveness

--- src/components/ProjectCardInfo.js
+++ src/components/ProjectCardInfo.js
@@ -4,13 +4,17 @@ import React from 'react'
 const ProjectCardInfo = ({title, tech, description, images, directory, url, status}) => {
     
     const displayTech = () => {
-        return tech.map( t => (` ${t} - `))
+        return tech.map( t => {
+            return (tech.indexOf(t) !== tech.length - 1 )
+            ? ` ${t} - `
+            : t
+        })
     }
 
     return (
         <div className={'project-info'} >
             <div className={'preview-title'}> {title}</div>
-            <div className={'preview-tech'}> - {displayTech()}</div>
+            <div className={'preview-tech'}> {displayTech()}</div>
             <span className={'preview-description'}>
                 <hr />
                 {description}
