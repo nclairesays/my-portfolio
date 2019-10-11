f43a2fa51af4dc453bfc09e433c7084d55fbbce4
nclairesays <45305029+nclairesays@users.noreply.github.com>
2019-10-10 17:13:59 -0700

    added clickable profile photos

--- src/components/ProfilePic.js
+++ src/components/ProfilePic.js
@@ -1,7 +1,32 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+
+const images = require.context("../../public/profile-pics", true);
+const pic1 = images("./profile1.jpg");
+const pic2 = images("./profile2.jpg");
+const pic3 = images("./profile3.jpg");
+const pic4 = images("./profile4.jpg");
+const pic5 = images("./profile5.jpg");
+const pic6 = images("./profile6.jpg");
 
 const ProfilePic = () => {
-    return  <div id={'profilepic'}  />
-}
+  const [picNumber, setPicNumber] = useState(0);
+  useEffect(() => {
+    if (picNumber === 6) {
+      setPicNumber(0);
+    }
+  });
+  const picList = [pic1, pic2, pic3, pic4, pic5, pic6];
+  const picImageStyle = i => {
+    return { backgroundImage: "url(" + picList[i] + ")" };
+  };
 
-export default ProfilePic
+  return (
+    <div
+      id={"profilepic"}
+      onClick={() => setPicNumber(picNumber + 1)}
+      style={picImageStyle(picNumber)}
+    />
+  );
+};
+
+export default ProfilePic;
